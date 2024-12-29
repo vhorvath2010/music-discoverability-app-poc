8a31edbd7a0e7d207a1f5f2a744ff56cc627571c
Vince <vhorvath2010@gmail.com>
2024-12-28 22:57:13 -0500

    fix: should use use for serving static content

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ import { serveStatic } from "@hono/hono/deno";
 
 const app = new Hono();
 
-app.get("*", serveStatic({ root: "./public" }));
+app.use("*", serveStatic({ root: "./public" }));
 
 Deno.serve(app.fetch);
