64bb581954125d4ecbf9fd4436bda2c2c26b4f00
Vince <vhorvath2010@gmail.com>
2024-12-29 22:31:10 -0500

    chore: starter creators in lieu of real DB

--- src/business/creatorRepository.ts
+++ src/business/creatorRepository.ts
@@ -1,7 +1,26 @@
 import { Creator } from "./creator.ts";
 
 export class CreatorRepository {
-  private creators: Creator[] = [];
+  private creators: Creator[] = [
+    {
+      name: "The Strokes",
+      location: "New York, NY, USA",
+      description:
+        "A New York-based band that pioneered the garage rock revival of the early 2000s with catchy riffs, raw vocals, and a cool, effortless style.",
+    },
+    {
+      name: "LCD Soundsystem",
+      location: "New York, NY, USA",
+      description:
+        "A dance-punk outfit known for blending electronic beats with sharp lyrics, nostalgic melodies, and James Murphy's distinctive vocal style.",
+    },
+    {
+      name: "Arctic Monkeys",
+      location: "Sheffield, UK",
+      description:
+        "A British rock band blending witty lyricism, energetic riffs, and evolving sounds from indie rock to lounge-inspired crooning.",
+    },
+  ];
 
   register(creator: Creator) {
     this.creators.push(creator);
